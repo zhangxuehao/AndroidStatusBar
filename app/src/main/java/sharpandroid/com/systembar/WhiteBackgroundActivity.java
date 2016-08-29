b92007a35fcc41ec5cb3a0cc0c9eccae8520c769
张学昊 <3305608683@qq.com>
2016-08-29 15:47:14 +0800

    white background style

--- app/src/main/java/sharpandroid/com/systembar/WhiteBackgroundActivity.java
+++ app/src/main/java/sharpandroid/com/systembar/WhiteBackgroundActivity.java
@@ -3,6 +3,7 @@ package sharpandroid.com.systembar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 /**
  * Created by jerry on 8/29/16.
@@ -13,6 +14,7 @@ public class WhiteBackgroundActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_white);
         StatusBarUtil.StatusBarLightMode(this);
         StatusBarUtil.setStatusBarColor(this, R.color.white);
